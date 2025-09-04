0c54cc27b5f124d16becde37753ee0287d2ee544
yukiko <neigeenfant32@gmail.com>
2025-09-04 13:42:28 +0900

    ローディング画面とレイアウト（横並び）を実装

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function createEl(parent, type, value) {
   const tag = nodeOps.create(type);
   if (type === 'img') {
     nodeOps.setAttr(tag, 'src', value);
-  } else if (type === 'li') {
+  } else {
     nodeOps.html(tag, value);
   }
   nodeOps.append(parent, tag);
@@ -50,8 +50,8 @@ function assemblyEl(json) {
 
     const ul = nodeOps.create('ul');
     createEl(ul, 'img', `${pass}${character.image}`);
-    createEl(ul, 'li', character.name);
-    createEl(ul, 'li', character.category);
+    createEl(ul, 'p', character.name);
+    createEl(ul, 'p', character.category);
 
     const li = nodeOps.create('li');
     nodeOps.append(li, ul);
@@ -59,28 +59,31 @@ function assemblyEl(json) {
   }
 }
 
-const spinner = document.getElementById("loading");
+const spinner = nodeOps.qs('.spinner', body);
+const load = nodeOps.qs('.load', body);
 function loading() {
-  spinner.classList.remove("loaded");
+  spinner.classList.remove('loaded');
+  load.classList.remove('loaded');
 }
 
 function loaded() {
-  spinner.classList.add("loaded");
+  spinner.classList.add('loaded');
+  load.classList.add('loaded');
 }
 
 async function drawList(url) {
-  nodeOps.html(listContainer, "");  // 画面をクリア
   loading();
   try {
-    const json = await Promise.all([
-      fetchCharacters(url),
-      new Promise(resolve => setTimeout(resolve, 600)) // ローディング待機
-    ]);
-    assemblyEl(json[0]);
+    const json = await fetchCharacters(url);
+    setTimeout(() => {
+      nodeOps.html(listContainer, "");
+      assemblyEl(json);
+      loaded();
+    }, 800);
   } catch (e) {
     console.error(e);
+    loaded();
   }
-  loaded();
 }
 
 
